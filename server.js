445ba357024221e4f184ac8e5fde60314177f337
Arlovzki <arloudev@gmail.com>
2021-10-17 15:23:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/mean-app'));
+app.use(express.static(__dirname + '/dist/message-app'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/mean-app/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/message-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
